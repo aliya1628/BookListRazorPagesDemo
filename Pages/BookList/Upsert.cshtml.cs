3906da3bb7f5d90d6810a8df288ee7987ca2c761
Codeest1628 <54244127+Codeest1628@users.noreply.github.com>
2019-10-16 12:08:33 +0530

    Added Upsert
    Upsert provides insert and update in one all together razor page

---
+++ Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazorPagesDemo.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazorPagesDemo.Pages.BookList
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public UpsertModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            Book = new Book();
+            if(id == null)
+            {
+                //create
+                return Page();
+            }
+
+            //update
+            Book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
+            if(Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                if(Book.Id == 0)
+                {
+                    _context.Book.Add(Book);
+                }
+                else
+                {
+                    _context.Book.Update(Book);
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
